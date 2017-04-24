53e7f4c570cc4798330569e77c4efa9a775c0edc
qinjb <qinjb@boqii.com>
2017-04-24 08:42:54 +0000

    增加Home模块

---
+++ app/Http/Controllers/Home/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Controllers\Home;
+
+use App\Http\Controllers\Controller;
+/**
+ * 首页控制情
+ *
+ * 
+ */
+class IndexController extends Controller
+{
+
+}
---
+++ app/Http/Controllers/Home/MessageController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Controllers\Home;
+
+use App\Http\Controllers\Controller;
+/**
+ * 留言控制情
+ *
+ * 
+ */
+class MessageController extends Controller
+{
+
+}
