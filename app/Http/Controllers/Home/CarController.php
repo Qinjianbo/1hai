bf4bb65c1b90df8d06bb484883e1ac1d40f83585
JianboQin <279250819@qq.com>
2017-05-13 22:51:33 +0800

    车辆详情

--- app/Http/Controllers/Home/CarController.php
+++ app/Http/Controllers/Home/CarController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
 use App\Models\Car\Car;
+use App\Models\Car\Rent;
 use App\Models\City\City;
 use App\Models\Shop\Shop;
 use Illuminate\Http\Request;
@@ -16,6 +17,14 @@ class CarController extends Controller
     public function index(Request $request)
     {
         $car = Car::find($request->get('car_id',0));
+        $rents = Rent::where('car_id', $request->get('car_id', 0))->get();
+        $cities = collect();
+        $shops = collect();
+        if (!$rents->isEmpty()) {
+            $cities = City::whereIn('id', $rents->pluck('city_id'))->get()->keyBy('id');
+            $shops = Shop::whereIn('id', $rents->pluck('shop_id'))->get()->keyBy('id');
+        }
+
         $carCount = Car::where('valid', 1)->count();
         $shopCount = Shop::count();
         $cityCount = City::count();
@@ -24,6 +33,9 @@ class CarController extends Controller
             'Home.car',
             [
                 'car' => $car,
+                'cities' => $cities,
+                'shops' => $shops,
+                'rents' => $rents,
                 'carCount'  => $carCount,
                 'shopCount' => $shopCount,
                 'cityCount' => $cityCount,
--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -81,9 +81,9 @@
 			<div class="container">
 				<div class="advantage">
 					<ul class="clearfix">			 
-						<li>{{ $cityCount }}多座城市</li>
-						<li>{{ $shopCount }}多服务网点</li>
-						<li>{{ $carCount }}余种车型</li>
+						<li>{{ $cityCount }}座城市</li>
+						<li>{{ $shopCount }}服务网点</li>
+						<li>{{ $carCount }}种车型</li>
 						<li>24小时服务</li>
 						<li>国际租车</li>
 					</ul>
