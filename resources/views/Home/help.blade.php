9424b9ddf0df0525b5f1de7729f744e4c004ad5e
JianboQin <279250819@qq.com>
2017-05-29 14:55:01 +0800

    底部公共样式提出来

--- resources/views/Home/help.blade.php
+++ resources/views/Home/help.blade.php
@@ -108,22 +108,7 @@
 		</script>
 		<div/>
 		<!--尾部	-->
-		<div class="footer">
-			<div class="container">
-				<div class="advantage">
-					<ul class="clearfix">			 
-						<li>{{ $cityCount }}座城市</li>
-						<li>{{ $shopCount }}服务网点</li>
-						<li>{{ $carCount }}种车型</li>
-						<li>24小时服务</li>
-						<li>国际租车</li>
-					</ul>
-				</div>
-			</div>
-			<div>
-				<p>版权所有&copy吴涛租车平台</p>
-			</div>
-		</div>
+			@include('Home.footer')
 		<script type="text/javascript" src="/js/index/index.js" ></script>
 		</div>
 	</body>
